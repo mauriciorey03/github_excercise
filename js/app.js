95f1962e9d64128256d96e0552af42f57343ca8c
Nicolas Suarez <brayansuarezmongui@gmail.com>
2024-02-23 19:41:55 -0500

    Cambios en el index y app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 //alert("Sitioweb");
-console.log("La app funciona de acuerdo a los requerimientos");
+console.log("La app funciona de acuerdo a los ");
+
+const divNombres = document.querySelector(".nombres")
+let nombres = ["Mauricio Meza", "Nicolas Suarez", "Diego Mancilla", "Jefferson Beltran"]
+
+nombres.forEach(nombre => {
+    divNombres.append(document.createTextNode(nombre + "\n"))
+})
